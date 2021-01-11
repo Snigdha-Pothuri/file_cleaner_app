94536487eb3433a39842871a0b267673a8f2ae65
Snigdha_13 <65667637+Snigdha-Pothuri@users.noreply.github.com>
2021-01-11 20:19:47 +0530

    Add files via upload

--- RemoveFiles.py
+++ RemoveFiles.py
@@ -1,3 +1,10 @@
 import os
 import shutil
 import time
+def main():
+    file=input("Which file do you want to delete:")
+    path=file
+    def get_file_or_folder_age(path):
+        days=30
+        seconds=time.time()-(days*24*60*60)
+        
